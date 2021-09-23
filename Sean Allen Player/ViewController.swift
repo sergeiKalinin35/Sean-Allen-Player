426d32a0b7adf7bf4c4781efbb57cb316703ecfe
Sergey <piolersa85@mail.ru>
2021-09-23 15:33:52 +0200

    Initial Commit

---
+++ Sean Allen Player/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sean Allen Player
+//
+//  Created by Sergey on 23.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
