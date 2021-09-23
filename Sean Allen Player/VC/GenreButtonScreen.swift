278f246a592bbbe725e3f0554dcf81de3164ca59
Sergey <piolersa85@mail.ru>
2021-09-23 18:17:00 +0200

    real iPhone

--- Sean Allen Player/VC/GenreButtonScreen.swift
+++ Sean Allen Player/VC/GenreButtonScreen.swift
@@ -25,21 +25,39 @@ class GenreButtonScreen: UIViewController {
     
     @IBAction func genreButtonTapped(_ sender: UIButton) {
         
-        
-        
+        MPMediaLibrary.requestAuthorization { (status) in
+            if status == .authorized {
+                DispatchQueue.main.async {
+                    self.playGenre(genre: sender.currentTitle!)
+                }
+               
+            }
+        }
     }
     
-   
-    @IBAction func stopButtonTapped(_ sender: UIButton) {
+   @IBAction func stopButtonTapped(_ sender: UIButton) {
+    musicPlayer.stop()
+    }
+    
+   @IBAction func nextButtonTapped(_ sender: UIButton) {
+    musicPlayer.skipToNextItem()
+    }
+    
+    
+    func playGenre(genre: String) {
+        
+        musicPlayer.stop()
+        
+        let query = MPMediaQuery()
+        let predicate = MPMediaPropertyPredicate(value: genre, forProperty: MPMediaItemPropertyGenre)
+        
+        query.addFilterPredicate(predicate)
+        
+        musicPlayer.setQueue(with: query)
+        musicPlayer.shuffleMode = .songs
+        musicPlayer.play()
         
     }
-    
-    
-    @IBAction func nextButtonTapped(_ sender: UIButton) {
-    }
-    
-    
-    
 
  
 }
