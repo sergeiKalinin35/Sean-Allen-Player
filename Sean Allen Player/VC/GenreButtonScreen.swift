4484538dec53ced277ed2a0914390c8f8e39db7f
Sergey <piolersa85@mail.ru>
2021-09-23 17:22:38 +0200

    Music Player

---
+++ Sean Allen Player/VC/GenreButtonScreen.swift
@@ -0,0 +1,45 @@
+//
+//  GenreButtonScreen.swift
+//  Sean Allen Player
+//
+//  Created by Sergey on 23.09.2021.
+//
+
+import UIKit
+import MediaPlayer
+
+
+
+
+class GenreButtonScreen: UIViewController {
+    
+    var musicPlayer = MPMusicPlayerController.applicationMusicPlayer
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func genreButtonTapped(_ sender: UIButton) {
+        
+        
+        
+    }
+    
+   
+    @IBAction func stopButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    
+    @IBAction func nextButtonTapped(_ sender: UIButton) {
+    }
+    
+    
+    
+
+ 
+}
